f7e3a8df1685ad12edadb2a3642f955191b405bf
pietrodev93 <pietrogiordano93@gmail.com>
2022-02-07 22:17:13 +0100

    Another test 7

--- util/dbConnect.js
+++ util/dbConnect.js
@@ -1,39 +1,19 @@
 import mongoose from 'mongoose'
 
+const connection = {};
 const MONGO_URL = process.env.MONGO_URL
 
-if (!MONGO_URL) {
-  throw new Error(
-  )
-}
-
-/**
- * Global is used here to maintain a cached connection across hot reloads
- * in development. This prevents connections growing exponentially
- * during API Route usage.
- */
-let cached = global.mongoose
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null }
-}
-
-async function dbConnect() {
-  if (cached.conn) {
-    return cached.conn
-  }
-
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
+async function dbConnect(){
+    if(connection.isConnected){
+        return;
     }
+    const db = await mongoose.connect(MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
 
-    cached.promise = mongoose.connect(MONGO_URL, opts).then((mongoose) => {
-      return mongoose
-    })
-  }
-  cached.conn = await cached.promise
-  return cached.conn
+    connection.isConnected = db.connections[0].readyState;
+    console.log(connection.isConnected);
 }
 
 export default dbConnect
