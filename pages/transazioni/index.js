0ff695dc5d01b15d7648ff2e6561c1e929f20613
pietrodev93 <pietrogiordano93@gmail.com>
2022-02-07 21:39:21 +0100

    Added url

--- pages/transazioni/index.js
+++ pages/transazioni/index.js
@@ -11,7 +11,7 @@ export default function Transazioni() {
   },[])
 
   const init = async () => {
-    const urlTransactions = `https://transactions-pietro.herokuapp.com/api/transactions`
+    const urlTransactions = `https://diario-finanziario.vercel.app/api/transactions`
     const { data } = await axios.get(urlTransactions, {
       headers: {
         'Access-Control-Allow-Origin': '*',
