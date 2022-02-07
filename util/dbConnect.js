ccf46d5e606fd249ee309a534b3cd0a5ac3d4209
pietrodev93 <pietrogiordano93@gmail.com>
2022-02-07 22:06:08 +0100

    Another test 3

--- util/dbConnect.js
+++ util/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = "mongodb+srv://pretuzz:pretuzz@cluster0.quzpg.mongodb.net/pizza?retryWrites=true&w=majority"
 
 if (!MONGODB_URI) {
   throw new Error(
