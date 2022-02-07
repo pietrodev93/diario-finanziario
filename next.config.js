ada879483ddb08a7e7539a1a0f0612a2a9e7d815
pietrodev93 <pietrogiordano93@gmail.com>
2022-02-07 20:57:56 +0100

    added db name

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    env: {
+      MONGODB_URI: "mongodb+srv://pretuzz:pretuzz@cluster0.quzpg.mongodb.net/pizza?retryWrites=true&w=majority",
+      MONGODB_NAME: "pizza"
+    },
+};
