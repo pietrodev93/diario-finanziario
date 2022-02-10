f385eb537a675c90c0ad0ff9136ee4ae6f703dbe
pietrodev93 <pietrogiordano93@gmail.com>
2022-02-10 18:56:37 +0100

    prova 1

--- util/dbConnect.js
+++ util/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 const connection = {};
-const MONGO_URL = process.env.MONGO_URL
+const MONGO_URL = process.env.MONGODB_URI
 
 async function dbConnect(){
     if(connection.isConnected){
