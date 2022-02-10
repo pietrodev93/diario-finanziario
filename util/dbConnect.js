8b6088e387e55702c17fdd70fea5fa53be3ec6e3
pietrodev93 <pietrogiordano93@gmail.com>
2022-02-10 19:13:03 +0100

    new dbConnect

--- util/dbConnect.js
+++ util/dbConnect.js
@@ -3,39 +3,17 @@ import mongoose from 'mongoose'
 const connection = {};
 const MONGO_URL = process.env.MONGODB_URI
 
-if (!MONGO_URL) {
-  throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local'
-  )
-}
-
-/**
- * Global is used here to maintain a cached connection across hot reloads
- * in development. This prevents connections growing exponentially
- * during API Route usage.
- */
-let cached = global.mongoose
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null }
-}
-
-async function dbConnect() {
-  if (cached.conn) {
-    return cached.conn
-  }
-
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
+async function dbConnect(){
+    if(connection.isConnected){
+        return;
     }
+    const db = await mongoose.connect(MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
 
-    cached.promise = mongoose.connect(MONGO_URL, opts).then((mongoose) => {
-      return mongoose
-    })
-  }
-  cached.conn = await cached.promise
-  return cached.conn
+    connection.isConnected = db.connections[0].readyState;
+    console.log(connection.isConnected);
 }
 
 export default dbConnect
